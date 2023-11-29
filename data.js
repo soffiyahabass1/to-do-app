3ea8613f756e0b46495da059d76c91aa4a3f282d
Hargent <yemisi3385@gmail.com>
2023-11-29 15:42:06 +0100

    fix: re-structured the app structure

---
+++ data.js
@@ -0,0 +1,32 @@
+const dummyTasks = [
+  {
+    title: "Test Task",
+    description: "This is just a test task",
+    date: "",
+    isCompleted: false
+  },
+  {
+    title: "Test Task",
+    description: "This is just a test task",
+    date: "",
+    isCompleted: false
+  },
+  {
+    title: "Test Task",
+    description: "This is just a test task",
+    date: "",
+    isCompleted: false
+  },
+  {
+    title: "Test Task",
+    description: "This is just a test task",
+    date: "",
+    isCompleted: false
+  },
+  {
+    title: "Test Task",
+    description: "This is just a test task",
+    date: "",
+    isCompleted: false
+  }
+];
