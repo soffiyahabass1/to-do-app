acfae1f7f11b82e5969c919ea98c3660805b22b1
Hargent <yemisi3385@gmail.com>
2023-11-30 08:33:42 +0100

    feat : created sign up and sign in forms

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+import parsley from "parsleyjs";
+
+const BASE_API_URL = `https://todo-fastapi-338k.onrender.com/api`;
 //! Dummy data pending the fetching time
 export const dummyTasks = [
   {
@@ -43,20 +46,113 @@ export const dummyTasks = [
     isCompleted: false
   }
 ];
+//! Sign up user
 
-// ! open and close modal
+const signUpForm = document.getElementById("signUpForm");
+// parsley.init(form);
+async function signUpUser({ signUpData }) {
+  console.log(signUpData, "is the signup data");
+  const signedUpUser = await fetch(`${BASE_API_URL}/api/users`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(signUpData)
+  });
+  return signedUpUser;
+}
+
+signUpForm.addEventListener("submit", function (event) {
+  event.preventDefault(); // Prevent the default form submission
+
+  // Get form data
+  const signUpFormData = new FormData(signUpForm);
+
+  // Convert FormData to a plain object
+  let signUpFormObject = {};
+  signUpFormData.forEach(function (value, key) {
+    signUpFormObject[key] = value;
+  });
+  const signedUpUser = signUpUser({ signUpData: signUpFormObject })
+    .then((user) => {
+      console.log(user);
+      return user;
+    })
+    .catch((err) => {
+      console.log(err);
+      return null;
+    });
+
+  // Log or use the form data as needed
+  // console.log("Sign up form Data:", signUpFormObject, signedUpUser);
+});
+
+//! Login user
+// document.addEventListener("DOMContentLoaded", function () {
+const loginForm = document.getElementById("loginForm");
+// parsley.init(form);
+loginForm.addEventListener("submit", function (event) {
+  event.preventDefault(); // Prevent the default form submission
+
+  // Get form data
+  const loginFormData = new FormData(loginForm);
+
+  // Convert FormData to a plain object
+  let loginFormObject = {};
+  loginFormData.forEach(function (value, key) {
+    loginFormObject[key] = value;
+  });
+
+  // Log or use the form data as needed
+  console.log("login form Data:", loginFormObject);
+});
+// });
+//! Getting all to do tasks
+async function getAllTodo() {
+  const todos = await fetch(`${BASE_API_URL}/todos`, {});
+}
+// ! open and close modals
 // the open modal button
-const openModalBtn = document.getElementById("open-modal");
-const closeModalBtn = document.getElementById("close-modal");
-const modal = document.getElementById("modal");
-openModalBtn.addEventListener("click", () => {
-  modal.classList.toggle("hidden");
-});
-closeModalBtn.addEventListener("click", () => {
-  modal.classList.toggle("hidden");
-});
-// the
+const modalOpenBtns = document.querySelectorAll(".open-modal");
+const modalCloseBtns = document.querySelectorAll(".close-modal");
+const modals = document.querySelectorAll(".modal");
+modalOpenBtns.forEach((btn) =>
+  btn.addEventListener("click", () => {
+    modals.forEach((modal) => {
+      // console.log(modal.id, btn.id);
+      if (`${modal.id}-modal` === btn.id) {
+        modal.classList.remove("hidden");
+      } else {
+        modal.classList.add("hidden");
+      }
+    });
+  })
+);
+modalCloseBtns.forEach((btn) =>
+  btn.addEventListener("click", () => {
+    modals.forEach((modal) => {
+      // console.log(modal.id, btn.id);
+      if (`close-${modal.id}` === btn.id) {
+        modal.classList.add("hidden");
+      } else {
+        return;
+      }
+    });
+  })
+);
+
+// const openModalBtn = document.getElementById("open-modal");
+// const closeModalBtn = document.getElementById("close-modal");
+// const modal = document.getElementById("modal");
+// openModalBtn.addEventListener("click", () => {
+//   modal.classList.toggle("hidden");
+// });
+// closeModalBtn.addEventListener("click", () => {
+//   modal.classList.toggle("hidden");
+// });
+
 // adding task
+
 // deleting task
 // editing task
 // opening task description
@@ -79,31 +175,18 @@ tabControls.forEach((control) => {
 // ! create tab components
 const completedTasks = dummyTasks.filter((task) => task.isCompleted);
 const unCompletedTasks = dummyTasks.filter((task) => !task.isCompleted);
-
-// const tasksContainer = document.getElementById("tasks");
-dummyTasks.forEach((task) => {
-  // console.log(task);
+const allTask = dummyTasks;
+allTask.forEach((task) => {
   const taskElement = createTaskElement(task);
   allTab.appendChild(taskElement);
-  // tasksContainer.appendChild(taskElement);
-  // const taskElementContainer = document.createElement("div");
-  // taskElementContainer.setAttribute("id", "all");
-  // return taskElementContainer;
 });
 completedTasks.forEach((task) => {
-  // console.log(task);
   const taskElement = createTaskElement(task);
   completedTab.appendChild(taskElement);
 });
 unCompletedTasks.forEach((task) => {
-  // console.log(task);
   const taskElement = createTaskElement(task);
   inProgressTab.appendChild(taskElement);
-  // const taskElementContainer = document.createElement("div");
-  // taskElementContainer.setAttribute("id", "in-progress"); // Set the id attribute
-
-  // taskElementContainer.appendChild(taskElement);
-  // return taskElementContainer;
 });
 function createTaskElement(task) {
   const taskElement = document.createElement("div");
